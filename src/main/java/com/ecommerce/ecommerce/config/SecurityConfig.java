b309f5cf6466cf2af3864edd237105d3c6087b08
LeandroZacariaz <leandrozacariaz18@gmail.com>
2025-05-24 16:10:42 -0300

    modificando securityconfig

--- src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:5173", "https://iconic-indumentaria.vercel.app/"
         ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
